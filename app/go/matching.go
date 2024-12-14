ea8a701031aca7ad8b81bbcdb3348ed9529445a6
Kentaro Mizuki <66548698+harsssh@users.noreply.github.com>
2024-12-15 04:56:59 +0900

    マッチングでトランザクションを貼る

--- app/go/matching.go
+++ app/go/matching.go
@@ -22,9 +22,16 @@ func matchingLoop(ctx context.Context, interval time.Duration) {
 
 // このAPIをインスタンス内から一定間隔で叩かせることで、椅子とライドをマッチングさせる
 func createMatch(ctx context.Context) {
+	tx, err := db.BeginTxx(ctx, nil)
+	if err != nil {
+		slog.Error(err.Error())
+		return
+	}
+	defer tx.Rollback()
+
 	// MEMO: 一旦最も待たせているリクエストに適当な空いている椅子マッチさせる実装とする。おそらくもっといい方法があるはず…
 	ride := &Ride{}
-	if err := db.GetContext(ctx, ride, `SELECT * FROM rides WHERE chair_id IS NULL ORDER BY created_at LIMIT 1`); err != nil {
+	if err := tx.GetContext(ctx, ride, `SELECT * FROM rides WHERE chair_id IS NULL ORDER BY created_at LIMIT 1`); err != nil {
 		if errors.Is(err, sql.ErrNoRows) {
 			//slog.Info("未マッチのライドがありません")
 			return
@@ -36,7 +43,7 @@ func createMatch(ctx context.Context) {
 	matched := &Chair{}
 	empty := false
 	for i := 0; i < 10; i++ {
-		if err := db.GetContext(ctx, matched, "SELECT * FROM chairs INNER JOIN (SELECT id FROM chairs WHERE is_active = TRUE ORDER BY RAND() LIMIT 1) AS tmp ON chairs.id = tmp.id LIMIT 1"); err != nil {
+		if err := tx.GetContext(ctx, matched, "SELECT * FROM chairs INNER JOIN (SELECT id FROM chairs WHERE is_active = TRUE ORDER BY RAND() LIMIT 1) AS tmp ON chairs.id = tmp.id LIMIT 1"); err != nil {
 			if errors.Is(err, sql.ErrNoRows) {
 				//slog.Info("空いている椅子がありません")
 				return
@@ -44,7 +51,7 @@ func createMatch(ctx context.Context) {
 			slog.Error(err.Error())
 		}
 
-		if err := db.GetContext(ctx, &empty, "SELECT COUNT(*) = 0 FROM (SELECT COUNT(chair_sent_at) = 6 AS completed FROM ride_statuses WHERE ride_id IN (SELECT id FROM rides WHERE chair_id = ?) GROUP BY ride_id) is_completed WHERE completed = FALSE", matched.ID); err != nil {
+		if err := tx.GetContext(ctx, &empty, "SELECT COUNT(*) = 0 FROM (SELECT COUNT(chair_sent_at) = 6 AS completed FROM ride_statuses WHERE ride_id IN (SELECT id FROM rides WHERE chair_id = ?) GROUP BY ride_id) is_completed WHERE completed = FALSE", matched.ID); err != nil {
 			slog.Error(err.Error())
 			return
 		}
@@ -57,7 +64,12 @@ func createMatch(ctx context.Context) {
 		return
 	}
 
-	if _, err := db.ExecContext(ctx, "UPDATE rides SET chair_id = ? WHERE id = ?", matched.ID, ride.ID); err != nil {
+	if _, err := tx.ExecContext(ctx, "UPDATE rides SET chair_id = ? WHERE id = ?", matched.ID, ride.ID); err != nil {
+		slog.Error(err.Error())
+		return
+	}
+
+	if err := tx.Commit(); err != nil {
 		slog.Error(err.Error())
 		return
 	}
